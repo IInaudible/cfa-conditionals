1afeccaffd1e021700758aa81ce76b9bdee68962
Kayla Golder <golderkayla@gmail.com>
2021-09-28 17:57:47 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const isGreater = (num) => {
+  //write a condition that checks if num1 is greater than num 2 and if it is return true else return false
+
+const isTeenager = (age) => {
+  //write a condition that checks if age is greater or equal to 13 and less than or equal to 19 if it is then return true, else return false
+}
+
+const favoriteSeason = (season) => {
+  //write a function that checks 
+  //if season is equal to "summer" if it is return "summer is your favorite season"
+  //otherwise check if season is equal to "spring" if it is return "spring is your favorite season"
+  //otherwise check if season is equal to "winter" if it is return "winter is your favorite season"
+  //otherwise check if season is equal to "fall" if it is return "fall is your favorite season"
+  //else return "input is not a season, try again"
+}
+  
