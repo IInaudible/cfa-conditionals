c3ee7617bad6646d0d9f45346f56b3f59519b45f
Kayla Golder <golderkayla@gmail.com>
2021-09-28 18:35:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-
+//1
 const isGreater = (num) => {
   //write a condition that checks if num1 is greater than num 2 and if it is return true else return false
+}
 
+//2
 const isTeenager = (age) => {
   //write a condition that checks if age is greater or equal to 13 and less than or equal to 19 if it is then return true, else return false
 }
 
+//3
 const favoriteSeason = (season) => {
   //write a function that checks 
   //if season is equal to "summer" if it is return "summer is your favorite season"
@@ -13,5 +16,23 @@ const favoriteSeason = (season) => {
   //otherwise check if season is equal to "winter" if it is return "winter is your favorite season"
   //otherwise check if season is equal to "fall" if it is return "fall is your favorite season"
   //else return "input is not a season, try again"
+}
+  
+
+//4
+  /* write a function determineGrade, that takes in one parameter, grade. That prints the grade of a person using the following conditions
+    ->A if percentage >85 and <= 100
+    ->A-if grade<=85 and >80
+    ->B if grade<=80 and >70
+    ->C if grade<=70 and >60
+    ->D if grade<=60 and >40
+    ->E if grade<=40 and >35
+    ->Failed if grade <= 35 should print 'candidate failed' 
+    else 'invalid grade entered
+  */
+// write your function under this comment ***
+
+//uncomment the line below once you have written your function
+//console.log(determineGrade(70) //should output C
 }
   
